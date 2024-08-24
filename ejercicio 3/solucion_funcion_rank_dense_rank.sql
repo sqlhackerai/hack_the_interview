8bef5c450d1ef3ef2637149cf7692d5bf7eaa536
kike <kike@MacBook-Pro-de-kike.local>
2024-08-23 23:19:11 -0600

    Agregando ejercicio 3

---
+++ ejercicio 3/solucion_funcion_rank_dense_rank.sql
@@ -0,0 +1,12 @@
+
+SELECT 
+    fecha,
+    categoria,
+    total,
+    RANK() OVER (PARTITION BY categoria ORDER BY total DESC) AS rank_ventas,
+    DENSE_RANK() OVER (PARTITION BY categoria ORDER BY total DESC) AS dense_rank_ventas,
+    ROW_NUMBER() OVER (PARTITION BY categoria ORDER BY total DESC) AS row_number_ventas
+FROM 
+    ventas
+ORDER BY 
+    categoria, rank_ventas;
