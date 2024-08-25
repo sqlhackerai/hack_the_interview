4d691a22a1b1f9ef73c6e98da1c40cedc5794201
Kike Davila <sq_hacker_ai@outlook.com>
2024-08-25 16:48:44 -0600

    fix: adding version 19

---
+++ ejercicio19/solucion_primero_ultimo_categoria.sql
@@ -0,0 +1,13 @@
+6SELECT 
+    categoria,
+    MIN(fecha) AS primera_venta_fecha,
+    MAX(fecha) AS ultima_venta_fecha,
+    FIRST_VALUE(total) OVER (PARTITION BY categoria ORDER BY fecha) AS primera_venta_total,
+    LAST_VALUE(total) OVER (PARTITION BY categoria ORDER BY fecha ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS ultima_venta_total,
+    LAST_VALUE(total) OVER (PARTITION BY categoria ORDER BY fecha ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) - FIRST_VALUE(total) OVER (PARTITION BY categoria ORDER BY fecha) AS diferencia_ventas
+FROM 
+    ventas
+GROUP BY 
+    categoria
+ORDER BY 
+    categoria;
